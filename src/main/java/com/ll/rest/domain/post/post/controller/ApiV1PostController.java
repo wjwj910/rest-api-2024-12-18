0b423b6733c4ed48129d02a117514530c63bf070
wjwj910 <wjwj910@gmail.com>
2025-01-08 15:04:32 +0900

    gen 2

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -80,7 +80,7 @@ public class ApiV1PostController {
 
         Post post = postService.write(reqBody.title, reqBody.content);
 
-        return new RsData(
+        return new RsData<Long>(
                 "200-1",
                 "%d번 글이 작성되었습니다.".formatted(post.getId()),
                 post.getId());
