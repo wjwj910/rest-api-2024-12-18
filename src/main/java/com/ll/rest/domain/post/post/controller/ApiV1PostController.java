495002285098cdaa770d3c78b3983e8d57e4b1a7
wjwj910 <wjwj910@gmail.com>
2025-01-08 12:02:08 +0900

    valid

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -3,7 +3,10 @@ package com.ll.rest.domain.post.post.controller;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
 import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,14 +36,19 @@ public class ApiV1PostController {
         return new RsData("200-1", "%d번 글을 삭제하였습니다.".formatted(id));
     }
 
-    record PostModifyReqBody(String title,
-                             String content) {
+    record PostModifyReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content) {
     }
 
     @PutMapping("/{id}")
     @Transactional
     public RsData modifyItem(@PathVariable long id,
-                             @RequestBody PostModifyReqBody reqBody) {
+                             @RequestBody @Valid PostModifyReqBody reqBody) {
         Post post = postService.findById(id).get();
 
         postService.modify(post, reqBody.title, reqBody.content);
