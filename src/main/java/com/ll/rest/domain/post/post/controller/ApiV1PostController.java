9a49d41cb519ff07b49d2143bb7fd4cf483405c0
wjwj910 <wjwj910@gmail.com>
2025-01-08 12:11:35 +0900

    post

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -55,4 +55,21 @@ public class ApiV1PostController {
 
         return new RsData("200-1", "%d번 글이 수정되었습니다.".formatted(id));
     }
+
+    record PostWriteReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content) {
+    }
+
+    @PostMapping
+    public RsData writeItem(@RequestBody @Valid PostWriteReqBody reqBody) {
+
+        Post post = postService.write(reqBody.title, reqBody.content);
+
+        return new RsData("200-1", "%d번 글이 작성되었습니다.".formatted(post.getId()));
+    }
 }
