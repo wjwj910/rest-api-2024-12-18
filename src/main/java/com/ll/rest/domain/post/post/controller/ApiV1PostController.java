ba7ec0b235e189f983f00d875867dc9f5c61eac8
wjwj910 <wjwj910@gmail.com>
2025-01-08 11:53:56 +0900

    record

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -3,8 +3,6 @@ package com.ll.rest.domain.post.post.controller;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
 import com.ll.rest.global.rsData.RsData;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -35,11 +33,8 @@ public class ApiV1PostController {
         return new RsData("200-1", "%d번 글을 삭제하였습니다.".formatted(id));
     }
 
-    @AllArgsConstructor
-    @Getter
-    public static class PostModifyReqBody {
-        private String title;
-        private String content;
+    record PostModifyReqBody(String title,
+                             String content) {
     }
 
     @PutMapping("/{id}")
@@ -48,7 +43,7 @@ public class ApiV1PostController {
                              @RequestBody PostModifyReqBody reqBody) {
         Post post = postService.findById(id).get();
 
-        postService.modify(post, reqBody.getTitle(), reqBody.getContent());
+        postService.modify(post, reqBody.title, reqBody.content);
 
         return new RsData("200-1", "%d번 글이 수정되었습니다.".formatted(id));
     }
