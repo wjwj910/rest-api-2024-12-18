57a88812dfe07dc1f55cf3cb674c317c35e688de
wjwj910 <wjwj910@gmail.com>
2025-01-08 15:16:38 +0900

    gen 5

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -36,11 +36,11 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public RsData deleteItem(@PathVariable long id) {
+    public RsData<Void> deleteItem(@PathVariable long id) {
         Post post = postService.findById(id).get();
         postService.delete(post);
 
-        return new RsData("200-1", "%d번 글을 삭제하였습니다.".formatted(id));
+        return new RsData<>("200-1", "%d번 글을 삭제하였습니다.".formatted(id));
     }
 
     record PostModifyReqBody(
