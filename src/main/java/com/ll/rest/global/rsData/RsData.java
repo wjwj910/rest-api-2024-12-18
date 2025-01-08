4b4470322cf7ee6cb7545309873d054458bfdee2
wjwj910 <wjwj910@gmail.com>
2025-01-08 14:39:23 +0900

    RsData, data 2

--- src/main/java/com/ll/rest/global/rsData/RsData.java
+++ src/main/java/com/ll/rest/global/rsData/RsData.java
@@ -1,16 +1,18 @@
 package com.ll.rest.global.rsData;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
 @Getter
 public class RsData {
     private String resultCode;
     private String msg;
-    private long data;
+    private Long data;
 
     public RsData(String resultCode, String msg) {
-        this(resultCode, msg, 0);
+        this(resultCode, msg, null);
     }
 }
