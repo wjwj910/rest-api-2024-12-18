9f2bc7b8640bd03e6a64d3700a497e50d9fcc25b
wjwj910 <wjwj910@gmail.com>
2025-01-08 16:49:26 +0900

    response entity remove 3

---
+++ src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
@@ -0,0 +1,42 @@
+package com.ll.rest.global.aspect;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ResponseAspect {
+    @Around("""
+            (
+                within
+                (
+                    @org.springframework.web.bind.annotation.RestController *
+                )
+                &&
+                (
+                    @annotation(org.springframework.web.bind.annotation.GetMapping)
+                    ||
+                    @annotation(org.springframework.web.bind.annotation.PostMapping)
+                    ||
+                    @annotation(org.springframework.web.bind.annotation.PutMapping)
+                    ||
+                    @annotation(org.springframework.web.bind.annotation.DeleteMapping)
+                )
+            )
+            ||
+            @annotation(org.springframework.web.bind.annotation.ResponseBody)
+            """)
+    public Object handleResponse(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("handleResponse 시작");
+
+        Object proceed = joinPoint.proceed();
+
+        System.out.println("handleResponse 끝");
+
+        return proceed;
+    }
+}
