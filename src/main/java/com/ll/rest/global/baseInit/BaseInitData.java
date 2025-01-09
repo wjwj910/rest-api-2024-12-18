8b349b982644516cb2a954eb9bf8e915601b4515
wjwj910 <wjwj910@gmail.com>
2025-01-09 10:40:11 +0900

    default_batch_fetch_size

--- src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -38,6 +38,8 @@ public class BaseInitData {
         Member memberUser1 = memberService.join("user1", "1234", "유저1");
         Member memberUser2 = memberService.join("user2", "1234", "유저2");
         Member memberUser3 = memberService.join("user3", "1234", "유저3");
+        Member memberUser4 = memberService.join("user4", "1234", "유저4");
+        Member memberUser5 = memberService.join("user5", "1234", "유저5");
     }
 
     @Transactional
@@ -46,9 +48,13 @@ public class BaseInitData {
 
         Member memberUser1 = memberService.findByUsername("user1").get();
         Member memberUser2 = memberService.findByUsername("user2").get();
+        Member memberUser3 = memberService.findByUsername("user3").get();
+        Member memberUser4 = memberService.findByUsername("user4").get();
 
         Post post1 = postService.write(memberUser1, "축구 하실 분?", "14시 까지 22명을 모아야 합니다.");
         Post post2 = postService.write(memberUser1, "배구 하실 분?", "15시 까지 12명을 모아야 합니다.");
         Post post3 = postService.write(memberUser2, "농구 하실 분?", "16시 까지 10명을 모아야 합니다.");
+        Post post4 = postService.write(memberUser3, "발야구 하실 분?", "17시 까지 14명을 모아야 합니다.");
+        Post post5 = postService.write(memberUser4, "피구 하실 분?", "18시 까지 18명을 모아야 합니다.");
     }
 }
